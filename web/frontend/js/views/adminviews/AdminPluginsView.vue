5900523670ced656cb284837dfd54d18a0d25ad6
et-nik <nikita.hldm@gmail.com>
2026-01-07 20:46:18 +0100

    plugins ui

---
+++ web/frontend/js/views/adminviews/AdminPluginsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <GBreadcrumbs :items="breadcrumbs"></GBreadcrumbs>
+</template>
+
+<script setup>
+import {GBreadcrumbs} from "@gameap/ui";
+import {computed} from "vue";
+import {trans} from "@/i18n/i18n";
+
+const breadcrumbs = computed(() => {
+  return [
+    {'route':'/', 'text':'GameAP', 'icon': 'gicon gicon-gameap'},
+    {'route':{name: 'admin.plugins.index'}, 'text':trans('plugins.plugins')},
+  ]
+})
+</script>
